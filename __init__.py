25f09983ca58041b794c148dd1526e8b2fa8b5a1
tedmlkskywalker <luffylecrack@gmail.com>
2025-12-12 09:21:56 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from urllib.request import urlopen
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +11,25 @@ def hello_world():
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen(
+        'https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx'
+    )
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15
+        results.append({
+            'Jour': dt_value,
+            'temp': temp_day_value
+        })
+
+    return jsonify(results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
